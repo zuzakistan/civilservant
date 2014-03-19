df3d59e9ad2b5f10096b177eac5c15400a86b523
Douglas Gardner <douglas@chippy.ch>
2014-03-19 20:42:51 +0000

    Nest nethack in its own command
    Nethack reporting will now only start on a `!nethack` command.

--- modules/noxbot.js
+++ modules/noxbot.js
@@ -1,25 +1,18 @@
 var bot = require('..');
 var irc = require('irc');
-var nox = new irc.Client(bot.config.irc.server,'civilservice',{
-  port:6667,channels:['#nethack']
-})
+bot.addListener('message', function (nick,to,tex,message) {
+  if(tex.substring(0,8) == "!nethack") {
+    var nox = new irc.Client(bot.config.irc.server,'civilservice',{
+      port:6667,channels:['#nethack']
+    })
+    bot.say(bot.config.irc.control,'Nethack reporting on.')
 
-nox.addListener('message', function (nick,to,text,message) {
-  if(nick == "Rodney") {
-    if ((text.indexOf('ross') != -1) || (text.indexOf('zuzak') != -1) || (text.indexOf('samstudio8') != -1) || (text.indexOf('danharibo') != -1)) {
-      if(text.indexOf('ross/') != -1) {
-        // disallow URLs that aren't pertinent
-        return
+    nox.addListener('message', function (nic,to,text,message) {
+      if(nic == "Rodney") {
+        if ((text.indexOf('ross ') != -1) || (text.indexOf('zuzak') != -1) || (text.indexOf('samstudio8') != -1) || (text.indexOf('danharibo') != -1)) {
+        bot.notice(bot.config.irc.control,text)
       }
-      bot.notice(bot.config.irc.control,text)
     }
-  }
-})
-nox.addListener('error', function (err) {
-  console.log(err);
-})
-bot.addListener('message', function (nick,to,text,message) {
-  if(text.substring(0,8) == "!nethack") {
-    bot.say('Rodney', '!' + text.substring(9))
+    })
   }
 })
