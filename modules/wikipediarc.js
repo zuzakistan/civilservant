8173441e73760a58d49db104c98accd0a26dc433
Douglas Gardner <douglas@chippy.ch>
2014-03-30 22:10:19 +0000

    fix RC filter

--- modules/wikipediarc.js
+++ modules/wikipediarc.js
@@ -5,10 +5,11 @@ var wikipedia = new irc.Client('irc.wikimedia.org','civilservant',{
 })
 
 wikipedia.addListener('message', function (nick,to,text,message) {
-  if ((text.indexOf('Aberystwyth') != -1) || text.indexOf('fig roll') != -1 || text.indexOf(' 144.124.') != -1) {
+  if ((text.indexOf('Aberystwyth') != -1) || text.indexOf('fig roll') != -1 || text.indexOf('144.124.') != -1) {
     bot.notice(bot.config.irc.control,text);
   }
 })
 wikipedia.addListener('error', function (err) {
   console.log(err);
 })
+console.log("Loaded RC reporting for English Wikipedia")
