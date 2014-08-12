41dfb0f83eb25f81dad51a468f9a051d01efbf19
Douglas Gardner <douglas@chippy.ch>
2014-08-12 16:12:56 +0000

    +

--- modules/languages.js
+++ modules/languages.js
@@ -1,5 +1,6 @@
 var bot = require('..');
 var langs = require( 'languages' );
+var countries = require( 'i18n-iso-countries' );
 
 bot.addListener('message', function (nick,to,text,message) {
   var args = text.split(' ');
@@ -12,10 +13,25 @@ bot.addListener('message', function (nick,to,text,message) {
 		bot.say(to,args[1] + ": " + info.name + " → " + info.nativeName);
 	} else {
 		if (args[1].length !== 2) {
-			bot.say(to,"Is that an ISO 639-1 code?");
+			if (args[1].length === 3) {
+				bot.say(to,"Sorry, nobody's bothered making an npm module for ISO 639-3.");
+			} else {
+				bot.say(to,"Is that an ISO 639-1 code?");
+			}
 		} else {
 			bot.say(to,args[1] + " is not recognised as an ISO 639-1 code.");
 		}
 	}
+  } else if ( args[0] == "!country" ) {
+	  if ( !args[1] ) {
+		  bot.say(to, "Usage: !country <code>");
+	  } else {
+		  var code = args[1].toUpperCase();
+		  if ( countries.getName(code, 'en') ) {
+			  bot.say(to, code + " → " + countries.getName(code, 'en') + ' · ' + countries.getName(code, 'de'));
+		  } else {
+			  bot.say(to, "Is that an ISO 3166-1 code?");
+		  }
+	  }
   }
 })
--- modules/wikilink.js
+++ modules/wikilink.js
@@ -4,31 +4,26 @@ var langs = require('languages');
 var iw = __dirname + '/interwikis.json';
 
 bot.addListener('message', function(nick,to,text,message){
-	var match = text.match(/\[\[.*]\]/);
-	if ( match) {
-		console.log('1 match');
-		match = match[0].substr(2,match[0].length-4);
+	var regexp = /\[\[(.*?)(?:\|.*?)?\]\]/g;
+	var match = regexp.exec( text );
+	if ( match ) {
+		match = match[1];
 		var namespace = 'w';
 		if ( match.split(':').length !== 1 ) {
-			console.log('2 iw');
 			match = match.split(':');
 			namespace = match.shift();
 			match = match.join(':');
 		}
-		console.log('3 reading');
 		fs.readFile(iw,'utf8',function(e,d){
-			console.log('4 read');
 			if(e){
 				console.log(JSON.stringify(e));
 				return;}
-			console.log('5');
 			d = JSON.parse(d);
-			console.log('6');
 			var str = '';
-			match = match.split(']]')[0];
 			if(d[namespace]) {
 				str = d[namespace].replace("$1",match);
 			} else {
+				match = match.charAt( 0 ).toUpperCase() + match.slice( 1 );
 				if ( langs.isValid(namespace) ) {
 					str = 'https://' + namespace + '.wikipedia.org/wiki/' + match;
 				} else {
