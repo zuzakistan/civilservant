6bb994c64c3f7d805e76ef18e9935fc8c7f1e9c0
Douglas Gardner <douglas@chippy.ch>
2014-08-20 07:06:57 +0000

    add kill

--- modules/basicfunctions.js
+++ modules/basicfunctions.js
@@ -3,8 +3,10 @@ var bot = require('..');
 bot.addListener('message', function (nick,to,text,message) {
   if ( text.substr(0,4) == "!say") {
     bot.notice(to, text.substr(text.indexOf(" ") + 1))
-  }
-  if ( text.substr(0,4) == "!src") {
+  } else if ( text.substr(0,4) == "!src") {
     bot.notice(to, nick + " : https://github.com/zuzakistan/civilservant");
+  } else if ( text.substr(0,4) == '!quit') {
+	  console.log('!quit requested by ' + nick + ' in ' + to);
+	  process.exit(0);
   }
 })
