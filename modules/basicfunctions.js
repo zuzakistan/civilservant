a93a064e7361f79e3628140b0e301719ba77d4e3
Douglas Gardner <douglas@chippy.ch>
2014-09-15 21:40:17 +0000

    use correct variable

--- modules/basicfunctions.js
+++ modules/basicfunctions.js
@@ -6,12 +6,18 @@ bot.addListener( 'message', function ( nick, to, text ) {
 	} else if ( text.substr( 0, 4 ) === '!src' ) {
 		bot.notice( to, nick + ' : https://github.com/zuzakistan/civilservant' );
 	} else if ( text.substr( 0, 5 ) === '!quit' ) {
-		if ( to === bot.config.irc.channel ) {
+		if ( to === bot.config.irc.control ) {
 			process.exit( 0 );
 		} else {
 			console.log( '!quit attempted by ' + nick + ' in ' + to  + ' (ignoring)');
 		}
 	} else if ( text === '!postcode' ) {
 		bot.say( to, nick + ': OX26 2FY' );
+	} else if ( text == '!control' ) {
+		if ( to == bot.config.irc.control ) {
+			bot.say( to, nick + ': this is the control channel' );
+		} else {
+			bot.say( to, nick + ': this is not the control channel' );
+		}
 	}
 } );
