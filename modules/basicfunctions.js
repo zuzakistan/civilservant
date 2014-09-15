aa57e79f3da21bc5d463f72232580b1a3664bf20
Douglas Gardner <douglas@chippy.ch>
2014-09-15 21:40:39 +0000

    rm errant space

--- modules/basicfunctions.js
+++ modules/basicfunctions.js
@@ -4,7 +4,7 @@ bot.addListener( 'message', function ( nick, to, text ) {
 	if ( text.substr( 0, 4 ) === '!say' ) {
 		bot.notice( to, text.substr( text.indexOf( ' ' ) + 1 ) );
 	} else if ( text.substr( 0, 4 ) === '!src' ) {
-		bot.notice( to, nick + ' : https://github.com/zuzakistan/civilservant' );
+		bot.notice( to, nick + ': https://github.com/zuzakistan/civilservant' );
 	} else if ( text.substr( 0, 5 ) === '!quit' ) {
 		if ( to === bot.config.irc.control ) {
 			process.exit( 0 );
@@ -13,8 +13,8 @@ bot.addListener( 'message', function ( nick, to, text ) {
 		}
 	} else if ( text === '!postcode' ) {
 		bot.say( to, nick + ': OX26 2FY' );
-	} else if ( text == '!control' ) {
-		if ( to == bot.config.irc.control ) {
+	} else if ( text === '!control' ) {
+		if ( to === bot.config.irc.control ) {
 			bot.say( to, nick + ': this is the control channel' );
 		} else {
 			bot.say( to, nick + ': this is not the control channel' );
