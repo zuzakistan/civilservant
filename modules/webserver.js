d52281cf65d7816c178d4efe601c4f8a75c8fefb
Douglas Gardner <douglas@chippy.ch>
2014-09-17 15:13:32 +0000

    escape, web hook

--- modules/webserver.js
+++ modules/webserver.js
@@ -2,8 +2,15 @@ var express = require( 'express' );
 var bot = require( '..' );
 var app = express();
 
+app.use( express.bodyParser() );
+
 app.get('/', function(req, res) {
 	res.json( {'message':'go away'} );
+} );
+
+app.post('/notification', function ( req, res ) {
+	bot.notice( bot.config.irc.control, '<' + req.connection.remoteAddress + '> ' + req.body.message.replace( /(?:\r\n|\r|\n)/g, '' ) );
+	res.json( {'message':'done'} );
 } );
 
 app.listen(bot.config.web.port);
