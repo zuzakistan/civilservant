94b3bfe7439b4b89d6d51a995078dac30ef33730
Douglas Gardner <douglas@chippy.ch>
2014-09-18 14:08:14 +0000

    refactor wikipedia watchlist

--- modules/wikipediarc.js
+++ modules/wikipediarc.js
@@ -1,20 +1,19 @@
-var bot = require('..');
-var irc = require('irc');
-var wikipedia = new irc.Client('irc.wikimedia.org','civilservant',{
+var bot = require( '..' );
+var irc = require( 'irc' );
+var wikipedia = new irc.Client( 'irc.wikimedia.org', 'civilservant', {
   port:6667,channels:[
 	'#en.wikipedia','#en.wikinews','#wikimania2014.wikimedia',
 	'#en.wiktionary','#en.wikibooks','#en.wikiversity','#en.wikisource',
 	'#en.wikivoyage','#meta.wikimedia','#commons.wikimedia']
-});
+} );
 
-wikipedia.addListener('message', function (nick,to,text) {
-  if ((text.indexOf('Aberystwyth') !== -1) || text.indexOf('fig roll') !== -1 || text.indexOf( '144.124.') !== -1) {
-    bot.notice(bot.config.irc.control,text);
-  } else if ((text.indexOf('Martin Smith (activist)') !== -1) || (text.indexOf('N3hima') !== -1)) {
-      bot.notice(bot.config.irc.control,text);
-  }
-});
-wikipedia.addListener('error', function (err) {
-  console.log(err);
-});
-console.log('Loaded RC reporting for English Wikipedia');
+wikipedia.addListener( 'message', function ( nick, to, text ) {
+	// mediawiki titles can never have a pipe in them, so this is almost safe
+	if ( new RegExp( bot.config.wikipedia.watchlist.join( '|' ) ).test( text ) ) {
+		bot.notice(bot.config.irc.control,text);
+	}
+} );
+wikipedia.addListener( 'error', function ( err ) {
+  console.log( err );
+} );
+console.log( 'Loaded RC reporting for English Wikipedia' );
