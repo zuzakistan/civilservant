9df22afd23d16db2452d184ff379fc3dadac7a0a
Douglas Gardner <douglas@chippy.ch>
2015-05-19 10:40:13 +0000

    add !watchlist command, filter out spammy messages

--- modules/wikipediarc.js
+++ modules/wikipediarc.js
@@ -7,7 +7,15 @@ var wikipedia = new irc.Client( 'irc.wikimedia.org', 'civilservant', {
 wikipedia.addListener( 'message', function ( nick, to, text ) {
 	// mediawiki titles can never have a pipe in them, so this is almost safe
 	if ( new RegExp( bot.config.wikipedia.watchlist.join( '|' ) ).test( text ) ) {
+		if ( text.indexOf( '[[Help:Cat-a-lot|Cat-a-lot]]:' ) !== -1 ) {
+			return;
+		}
 		bot.notice(bot.config.irc.control,text);
+	}
+} );
+bot.addListener( 'message', function ( nick, to, text ) {
+	if ( text === '!watchlist' ) {
+		bot.say( to, nick + ': ' + bot.config.wikipedia.watchlist.join( '; ' ) );
 	}
 } );
 wikipedia.addListener( 'error', function ( err ) {
