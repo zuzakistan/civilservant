fadd6a77c4ebd8f286edf47e0b2080e7278421e3
Douglas Gardner <douglas@chippy.ch>
2015-07-01 15:18:04 +0000

    continue shameless code reuse with !tld
    Adds !tld command to check whether a top level domain exists or not,
    by querying IANAs database, e.g. !tld com; !tld google.
    
    Does not support !tld .com, which it probably should.

---
+++ modules/tld.js
@@ -0,0 +1,26 @@
+var bot = require( '..' );
+var request = require( 'request' );
+bot.addListener( 'message', function ( nick, to, text ) {
+	var args = text.split( ' ' );
+	if ( args[0] === '!tld' ) {
+		if ( args.length !== 2 ) {
+			return bot.say( to, nick + ': usage e.g.: !tld com' );
+		}
+		request.get( 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt', function ( e, r, b ) {
+				if ( e ) {
+					return bot.say( to, nick + ': problem fetching data' );
+				}
+				if ( r.statusCode !== 200 ) {
+					return bot.say( to, nick + ': problem fetching data (' + r.statusCode + ')' );
+				}
+				var data = b.split( '\n' );
+				if ( data.indexOf( args[1].toUpperCase() ) !== -1  ) {
+					return bot.say( to, nick + ': ' + args[1] + ' is a TLD' );
+				} else {
+					return bot.say( to, nick + ': ' + args[1] + ' not found in TLD database' );
+				}
+			}
+		);
+	}
+} );
+
