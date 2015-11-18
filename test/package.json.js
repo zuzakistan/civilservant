ff14a910e54248d373d15842c9239de8b3c70591
Douglas Gardner <douglas@chippy.ch>
2015-11-18 03:33:29 +0000

    add authors/contributors to package.json

--- test/package.json.js
+++ test/package.json.js
@@ -1,5 +1,7 @@
 var pjson = require( '../package.json' );
+var _ = require( 'underscore' );
 var assert = require( 'assert' );
+var exec = require( 'child_process' ).exec;
 var grep = require( 'simple-grep' );
 
 var deps = pjson.dependencies;
@@ -10,5 +12,21 @@ describe( 'package.json', function () {
 				assert.notEqual( res.length, 0 );
 			} );
 		} );
+	} );
+
+	it( 'should credit all contributors', function ( done ) {
+		exec( 'git log --pretty=format:"%an <%ae>" | sort -u',
+			function ( err, stdout ) {
+				var committers = stdout.split( '\n' );
+				var authors = pjson.contributors || [];
+				authors.push( pjson.author || null);
+
+				var diff = _.difference( committers, authors );
+
+				// comes with ''
+				assert.equal( diff.length, 1 );
+				done();
+			}
+		);
 	} );
 } );
