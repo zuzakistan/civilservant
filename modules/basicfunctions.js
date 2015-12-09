808c00d3c4b9078ff21fc63994b7af78793004f6
Douglas Gardner <douglas@chippy.ch>
2015-12-09 22:02:00 +0000

    rm pointless substrs

--- modules/basicfunctions.js
+++ modules/basicfunctions.js
@@ -24,11 +24,11 @@ var bot = require( '..' );
 bot.addListener( 'message', function ( nick, to, text ) {
 	if ( text.substr( 0, 4 ) === '!say' ) {
 		bot.notice( to, text.substr( text.indexOf( ' ' ) + 1 ) );
-	} else if ( text.substr( 0, 4 ) === '!src' ) {
+	} else if ( text === '!src' ) {
 		bot.say( to, nick + ': https://github.com/zuzakistan/civilservant' );
-	} else if ( text.substr( 0, 5 ) === '!help' || text.substr( 0, 9 ) === '!commands' ) {
+	} else if ( text === '!help' || text === '!commands' ) {
 		bot.say( to, nick + ': https://github.com/zuzakistan/civilservant/blob/master/COMMANDS.md' );
-	} else if ( text.substr( 0, 5 ) === '!quit' ) {
+	} else if ( text  === '!quit' ) {
 		if ( to === bot.config.irc.control ) {
 			process.exit( 0 );
 		} else {
