b501b45247e5efe4f10817f2b1ae504baf64c88a
Douglas Gardner <douglas@chippy.ch>
2015-12-09 17:45:23 +0000

    add random nick

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var config = require( './config.json' );
+var nicks = require( './names.json' );
 
 var irc = require( 'irc' );
 
+if ( config.irc.nick === 'random' ) {
+	config.irc.nick = nicks[Math.floor( Math.random() * nicks.length)];
+}
+console.log(config.irc.nick);
 var bot = new irc.Client( config.irc.server, config.irc.nick, config.irc );
 
 bot.config = config;
