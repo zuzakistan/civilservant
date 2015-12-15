8de9f653d114d64318f5cfe0f312989707a86941
Douglas Gardner <douglas@chippy.ch>
2015-12-15 18:20:29 +0000

    improve khan

--- modules/khan.js
+++ modules/khan.js
@@ -5,12 +5,14 @@
  * Makes the bot say KHAAAAAAAAAAAAN.
  */
 var bot = require( '..' );
+var khan = require( 'khaan' );
 
 bot.addListener( 'message', function ( nick, to, text ) {
 	if ( text.substr( 0, 5 ) === '!khan' ) {
+		var args = text.split( ' ' );
+		var word = args[1] ? args[1] : 'khan';
 		var a = bot.config.khan || 434;
 		var number = Math.floor(Math.random() * a) + 1;
-		var A = Array(number).join('A');
-		bot.say( to, 'KH' + A + 'N' );
+		bot.say( to, khan.khan( word, number ) );
 	}
 } );
