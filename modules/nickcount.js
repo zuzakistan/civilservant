bd899bf7f42bc26fdb5b8430b5c1ec85e9cefabb
Douglas Gardner <douglas@chippy.ch>
2015-12-24 11:40:33 +0000

    add stats

--- modules/nickcount.js
+++ modules/nickcount.js
@@ -11,8 +11,13 @@ var fs = require( 'fs' );
 
 var nicks = require( __dirname + '/../data/channel-nicks.json' );
 
-bot.addListener( 'nick', function ( oldnick, newnick ){
-	nicks.push(newnick);
+bot.addListener( 'nick', function ( oldnick, newnick, channels ){
+	nicks.push({
+		'new': newnick,
+		'old': oldnick,
+		'ts': Date.now(),
+		'channels': channels
+	});
 	fs.writeFile( __dirname + '/../data/channel-nicks.json', JSON.stringify( nicks, null, 4 ) );
 } );
 
