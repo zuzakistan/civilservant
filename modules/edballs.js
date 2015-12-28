28db2714948475e88f822192dda0db93555b76e3
Douglas Gardner <douglas@chippy.ch>
2015-12-28 00:33:59 +0000

    add the greater good (the greater good)

--- modules/edballs.js
+++ modules/edballs.js
@@ -1,7 +1,8 @@
 module.exports = {
 	events: {
-		message: function ( bot, nick, to, text ) {
-			var pc = text.match( /ed balls/gi );
+		/* TODO: fix dupe bot var */
+		message: function ( _bot, bot, nick, to, text ) {
+			var pc = text.match( /(ed balls|the greater good)+/gi );
 			if ( pc && pc.length !== 0 ) {
 				bot.say( to, pc.join( ' ' ) );
 			}
