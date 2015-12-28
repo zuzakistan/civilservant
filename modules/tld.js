8fa59fc0608dba08e2e2ff79690207b16e57503d
Douglas Gardner <douglas@chippy.ch>
2015-12-28 12:25:23 +0000

    add TLD module once more

---
+++ modules/tld.js
@@ -0,0 +1,19 @@
+var request = require( 'request' );
+module.exports = {
+	commands: {
+		tld: {
+			usage: [ 'tld' ],
+			help: 'Checks whether a string is a top level domain',
+			command: function ( bot, msg ) {
+				request.get( 'https://data.iana.org/TLD/tlds-alpha-by-domain.txt', function ( e, r, b ) {
+					var data = b.split( '\n' );
+					if ( data.indexOf( msg.args.tld.toUpperCase() ) !== -1 ) {
+						bot.say( msg.to, msg.nick + ': ' + msg.args.tld + ' is a TLD' );
+					} else {
+						bot.say( msg.to, msg.nick + ': ' + msg.args.tld + ' is not in IANA\'s list of TLDs' );
+					}
+				} );
+			}
+		}
+	}
+};
