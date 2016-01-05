7eeda373df3a2177b4ef43537e1b5d874966f770
Douglas Gardner <douglas@chippy.ch>
2016-01-05 15:20:32 +0000

    Start standardising tumblr questions

---
+++ modules/cheese.js
@@ -0,0 +1,19 @@
+var scrape = require('scrape');
+module.exports = {
+	commands: {
+		cheese: {
+			help: 'Returns the Cheese of the Day',
+			command: function ( bot, msg ) {
+				scrape.request('http://www.cheese.com', function ( err, $ ) {
+					if ( err ) {
+						return;
+					}
+					$( 'div.top-offer' ).each( function ( div ) {
+						var a = div.find( 'a' ).first();
+						bot.say( msg.to, msg.nick + ': ' + a.text + ' http://www.cheese.com' + a.attribs.href );
+					} );
+				} );
+			}
+		}
+	}
+};
