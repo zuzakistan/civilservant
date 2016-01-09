caf4c0c301f687026aadfec1996a53d767874d67
Douglas Gardner <douglas@chippy.ch>
2016-01-09 21:07:59 +0000

    Add loud URL shortener

---
+++ modules/shortener.js
@@ -0,0 +1,16 @@
+var Bitly = require( 'bitly' );
+module.exports = {
+	events: {
+		url: function ( bot, url, nick, to ) {
+			var bitly = new Bitly( bot.config.bitly.username, bot.config.bitly.password );
+			bitly.shorten( url.href, function ( err, res ) {
+				if ( err ) {
+					return; // fail silently (usually duplicate URL)
+				}
+				if ( res.data.url ) {
+					bot.shout( to, res.data.url );
+				}
+			} );
+		}
+	}
+};
