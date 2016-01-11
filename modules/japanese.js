0f8efe813bee46f0ae310cebb967f3fbc3f94c7d
Douglas Gardner <douglas@chippy.ch>
2016-01-11 12:01:17 +0000

    Add romaji transliteration

---
+++ modules/japanese.js
@@ -0,0 +1,18 @@
+/**
+ * The original intention of this module was to translate
+ * using the Google Translate API, but it's a paid service.
+ * Hence, this pale imitation.
+ */
+var jp = require( 'japanese' );
+
+module.exports = {
+	events: {
+		message: function ( bot, nick, to, text ) {
+			// http://stackoverflow.com/a/15034560/1875784
+			if ( text.match( /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/ ) ) {
+				bot.shout( to, '\u000314' + jp.romanize( text ) );
+			}
+		}
+	}
+};
+
