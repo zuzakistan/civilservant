cae18c91f85fd6bed31a906a332cfac26785548c
Douglas Gardner <douglas@chippy.ch>
2016-01-28 17:38:43 +0000

    Document Ed Balls

--- modules/edballs.js
+++ modules/edballs.js
@@ -2,6 +2,12 @@ module.exports = {
 	events: {
 		/* TODO: fix dupe bot var */
 		message: function ( bot, nick, to, text ) {
+			/**
+			 * https://twitter.com/edballs/status/63623585020915713
+			 * http://i.imgur.com/aFhWSah.webm
+			 * https://www.youtube.com/watch?v=w4CQin03MDQ
+			 * http://thestanleyparable.wikia.com/wiki/Eight
+			 */
 			var pc = text.match( /(ed balls|the greater good|now we know|eight)+/gi );
 			if ( pc && pc.length !== 0 ) {
 				bot.shout( to, pc.join( ' ' ) );
