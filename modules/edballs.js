1825d28b311858e493b40d66a0592b5f4ec20006
Douglas Gardner <douglas@chippy.ch>
2016-01-28 02:29:47 +0000

    Fix #22

--- modules/edballs.js
+++ modules/edballs.js
@@ -2,7 +2,7 @@ module.exports = {
 	events: {
 		/* TODO: fix dupe bot var */
 		message: function ( bot, nick, to, text ) {
-			var pc = text.match( /(ed balls|the greater good|now we know)+/gi );
+			var pc = text.match( /(ed balls|the greater good|now we know|8|eight)+/gi );
 			if ( pc && pc.length !== 0 ) {
 				bot.shout( to, pc.join( ' ' ) );
 			}
