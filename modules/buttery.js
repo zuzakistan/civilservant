d586a37d47390b3c4f38439a6cee5d4508b05565
Douglas Gardner <douglas@chippy.ch>
2016-02-09 18:47:28 +0000

    Buttery biscuit base

---
+++ modules/buttery.js
@@ -0,0 +1,24 @@
+/**
+ * Eighteen seconds of spam.
+ */
+var readline = require( 'readline' );
+var fs = require( 'fs' );
+module.exports = {
+	commands: {
+		buttery: {
+			help: 'Sings a lovely song',
+			aliases: [ 'biscuit', 'base', 'wobble' ],
+			privileged: true, // very spammy!
+			command: function ( bot, msg ) {
+				var rl = readline.createInterface( {
+					input: fs.createReadStream( __rootdir + '/data/buttery.txt' ),
+					terminal: false
+				} );
+
+				rl.on( 'line', function ( line ) {
+					bot.say( msg.to, line );
+				} );
+			}
+		}
+	}
+};
