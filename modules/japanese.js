5332b6d22c81cf0701b175735970651c1af9807e
Douglas Gardner <douglas@chippy.ch>
2016-02-22 16:37:38 +0000

    Add trim to japanese check
    Fixes #28.

--- modules/japanese.js
+++ modules/japanese.js
@@ -11,7 +11,7 @@ module.exports = {
 			// http://stackoverflow.com/a/15034560/1875784
 			if ( text.match( /[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]/ ) ) {
 				var romaji = jp.romanize( text );
-				if ( romaji ) {
+				if ( romaji.trim()  ) {
 					bot.shout( to, '\u000314' + romaji );
 				}
 			}
