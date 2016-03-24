91ccbe741835d5565d26f25bcfd172eccef21751
Douglas Gardner <douglas@chippy.ch>
2016-03-24 10:57:35 +0000

    Fix? 'lol' handling

--- modules/lol.js
+++ modules/lol.js
@@ -1,8 +1,14 @@
 module.exports = {
 	events: {
 		message: function ( bot, nick, to, text ) {
-			if ( text === 'lol' && Math.random() > 0.8 ) {
-				if ( Math.random() > 0.8 ) {
+			if ( text === 'lol' && Math.random() > 0.8 ) { // 1 in 5
+				if ( Math.random() > 0.8 ) { // 1 in 25
+					if ( Math.random() > 0.8 ) { // 1 in 125
+						var interjections = [
+							'hahahaha XD'
+						];
+						bot.shout( to, interjections[Math.floor( Math.random() * interjections.length )] );
+					}
 					bot.shout( to, 'LOL' );
 				} else {
 					bot.shout( to, 'lol' );
