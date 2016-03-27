bf1845d2fe19899beae047984210c9728079fdef
Douglas Gardner <douglas@chippy.ch>
2016-03-27 20:42:34 +0000

    Remember question history

--- modules/tumblr.js
+++ modules/tumblr.js
@@ -1,18 +1,42 @@
+/**
+ * Select a random question from a list, and display it.
+ *
+ * If an ID is given, display that question.
+ * If it isn't, give the last question, plus one.
+ * If that's not possible, show a random question.
+ *
+ * This module needs rewriting.
+ */
 var fs = require( 'fs' );
+var id = null; // TODO do this properly
 module.exports = {
 	commands: {
 		tumblr: {
 			help: 'Returns a random question from a selection of viral Tumblr questions.',
 			aliases: [ 'truth', 'question', 'conversationprompt' ],
 			command: function ( bot, msg ) {
+				if ( msg.args[1] === 'clear' ) {
+					id = null;
+					return 'Wiped history.';
+				} else if ( msg.args[1] === 'set' ) {
+					if ( msg.args[2] ) {
+						id = parseInt( msg.args[2], 10 );
+						return 'Set counter to ' + id;
+					}
+					return 'Unable to set counter.';
+				}
 				var file = fs.readFileSync( __rootdir + '/data/tumblr.txt', { encoding: 'utf-8' } );
 				var questions = file.split( '\n' );
-				var index = msg.args[1] || Math.floor( Math.random() * questions.length );
+				var rand = Math.floor( Math.random() * questions.length );
+				var index = msg.args[1] || id || rand;
+				id = parseInt( index, 10 ) + 1;
 				var question = questions[index];
-				if ( question ) {
-					return '[Question ' + index + '] ' + questions[index];
+				if ( !question ) {
+					id = null;
+					question = questions[rand];
+					index = rand;
 				}
-				return 'Question not found.';
+				return '[Question ' + index + '/' + questions.length + '] ' + question;
 			}
 		}
 	}
