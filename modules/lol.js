4356358573c7c043a5a4cf3eee73408d93a9ddb5
Douglas Gardner <douglas@chippy.ch>
2016-03-27 19:21:18 +0000

    Laugh harder

--- modules/lol.js
+++ modules/lol.js
@@ -8,10 +8,21 @@ module.exports = {
 							'hahahaha XD'
 						];
 						bot.shout( to, interjections[Math.floor( Math.random() * interjections.length )] );
+					} else {
+						bot.shout( to, 'LOL' );
 					}
-					bot.shout( to, 'LOL' );
 				} else {
 					bot.shout( to, 'lol' );
+				}
+			} else if ( text === 'LOL' && Math.random() > 0.8 ) {
+				if ( Math.random() > 0.8 ) {
+					bot.shout( to, 'ＬＯＬ' );
+				} else {
+					if ( Math.random() > 0.4 ) {
+						bot.shout( to, 'LOL' );
+					} else {
+						bot.shout( to, 'lol' );
+					}
 				}
 			}
 		}
