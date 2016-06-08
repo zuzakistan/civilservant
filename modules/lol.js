4ccda2563a1375e0de61904e9df188b3d579aa4a
Douglas Gardner <douglas@chippy.ch>
2016-06-08 21:49:31 +0000

    Raise interjection chance

--- modules/lol.js
+++ modules/lol.js
@@ -3,7 +3,7 @@ module.exports = {
 		message: function ( bot, nick, to, text ) {
 			if ( text === 'lol' && Math.random() > 0.8 ) { // 1 in 5
 				if ( Math.random() > 0.8 ) { // 1 in 25
-					if ( Math.random() > 0.8 ) { // 1 in 125
+					if ( Math.random() > 0.5 ) { // 1 in 50
 						var interjections = [
 							'LMAO',
 							'ROFL',
