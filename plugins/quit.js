5dbd48d941e798607725d60d1f3165e6328304b9
Douglas Gardner <douglas@chippy.ch>
2016-09-29 10:18:54 +0000

    Add pretty quit message

---
+++ plugins/quit.js
@@ -0,0 +1,8 @@
+var bot = require( '..' );
+
+process.on( 'SIGINT', function () {
+	console.log( 'Caught SIGINT...' );
+	bot.disconnect( 'Caught ^C', function () {
+		console.log( '...disconnected.' );
+	} );
+} );
