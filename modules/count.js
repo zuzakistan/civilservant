414c9c928a1a5313172c9141985d4f0b32e4f000
Douglas Gardner <douglas@chippy.ch>
2016-10-25 13:41:04 +0000

    Add count command

---
+++ modules/count.js
@@ -0,0 +1,33 @@
+var fs = require( 'fs' );
+var count;
+try {
+	count = require( __rootdir + '/data/count.json' );
+} catch ( e ) {
+	count = {};
+}
+module.exports = {
+	commands: {
+		increment: {
+			aliases: [ 'count', '++' ],
+			help: 'Increments a number.',
+			command: function ( bot, msg ) {
+				var counter = msg.args.splice( 1 );
+				if ( counter === [] ) {
+					counter = ' '; // impossible to send ' ' as argument
+				} else {
+					counter = counter.join( ' ' );
+				}
+				if ( !count.hasOwnProperty(counter) ) {
+					count[counter] = 1;
+				} else {
+					count[counter]++;
+				}
+				fs.writeFile( __rootdir + '/data/count.json', JSON.stringify( count, null, 4 ) );
+				if ( counter === '' ) { // reuse :(
+					return '' + count[counter];
+				}
+				return counter + ' is now ' + count[counter];
+			}
+		}
+	}
+};
