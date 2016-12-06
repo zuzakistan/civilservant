46285eae0911cefb974a538845b3be3dfa108535
Douglas Gardner <douglas@chippy.ch>
2016-12-06 17:46:08 +0000

    Add RFC module

---
+++ modules/rfc.js
@@ -0,0 +1,14 @@
+/**
+ * Applies a Welsh soft mutation to a given string.
+ */
+module.exports = {
+	commands: {
+		rfc: {
+			help: 'Links to specified RFC',
+			usage: [ 'rfc' ],
+			command: function ( bot, msg ) {
+				return 'https://tools.ietf.org/html/rfc' + msg.args.rfc;
+			}
+		}
+	}
+};
