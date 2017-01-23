b6028651248106b91f1c9118d737b1b9c51df2f5
Douglas Gardner <douglas@chippy.ch>
2017-01-23 15:10:46 +0000

    Add timezone

---
+++ modules/timezones.js
@@ -0,0 +1,29 @@
+var moment = require( 'moment-timezone' );
+module.exports = {
+	commands: {
+		tz: {
+			help: 'Get the time in a given time zone',
+			alias: 'tz',
+			usage: 'foo',
+			command: function ( bot, msg ) {
+				var output = [];
+				if ( msg.args.length === 1 ) {
+					msg.args = [
+						msg.args[0],
+						'America/Los_Angeles',
+						'America/New_York',
+						'Europe/London',
+						'Europe/Paris',
+						'Europe/Moscow',
+						'Asia/Shanghai',
+						'Asia/Tokyo'
+					];
+				}
+				for ( var i = 1 /* ! */; i < msg.args.length; i++ ) {
+					output.push( moment( + new Date() ).tz( msg.args[i] ).format( 'h[h]mma z' ) );
+				}
+				return output.join( ' · ' );
+			}
+		}
+	}
+};
