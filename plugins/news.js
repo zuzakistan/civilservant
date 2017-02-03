c7ec3f756708fe76c9a1a74b5009d59faa0a555c
Douglas Gardner <douglas@chippy.ch>
2017-02-03 18:48:51 +0000

    Pull multiple BBC News feeds for various regions

--- plugins/news.js
+++ plugins/news.js
@@ -1,16 +1,24 @@
-var NEWS_URL = 'http://polling.bbc.co.uk/news/latest_breaking_news?audience=Domestic';
+var NEWS_URL = 'http://polling.bbc.co.uk/news/latest_breaking_news?audience=';
+var NEWS_AUDIENCES = [
+	'Domestic',
+	'International',
+	'US'
+];
 var request = require( 'request' );
 
 var bot = require( '..' );
 
-var poll = function () {
-	request( NEWS_URL, function ( err, res, body ) {
+var poll = function ( audience ) {
+	request( NEWS_URL + audience, function ( err, res, body ) {
 		var data = JSON.parse( body );
 		if ( data.html !== '' ) {
-			bot.fireEvents( 'rawnews', data.html );
+			bot.fireEvents( 'rawnews', data.html, audience );
 		}
 		setTimeout( poll, data.pollPeriod );
 	} );
 };
 
-poll();
+for ( var i = 0; i < NEWS_AUDIENCES.length; i++ ) {
+	console.log( 'Polling ' + NEWS_AUDIENCES[i] + ' BBC News' );
+	poll( NEWS_AUDIENCES[i] );
+}
