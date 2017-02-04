ac2ebcf8857c7da3cc00367effc02cce794532bc
Douglas Gardner <douglas@chippy.ch>
2017-02-04 23:20:05 +0000

    Add additional DNS functionality

--- modules/dns.js
+++ modules/dns.js
@@ -2,23 +2,35 @@ var dns = require( 'dns' );
 
 module.exports = {
 	commands: {
-		dns: {
-			aliases: [ 'rdns', 'host' ],
-			help: 'Attempts to look up a domain in the DNS system.',
-			usage: [ 'host' ],
+		rdns: {
+			aliases: [ 'host' ],
+			help: 'Attempts to look up a domain from an IP address.',
+			usage: [ 'ip' ],
 			command: function ( bot, msg ) {
 				try {
-					dns.reverse( msg.args.host, function ( err, data ) {
+					dns.reverse( msg.args.ip, function ( err, data ) {
 						if ( err ) {
-							return err + ' ' + msg.args.host;
+							bot.say( msg.to, 'Error:' + err + ' ' + msg.args.ip );
 						}
 						for ( var i = 0; i < data.length; i++ ) {
-							return msg.args.host + ' → ' + data[i];
+							bot.say( msg.to, msg.args.ip + ' → ' + data[i] );
 						}
 					} );
 				} catch ( e ) {
 					return e.message;
 				}
+			}
+		},
+		dns: {
+			help: 'Attempts to look up an IP address from a domain.',
+			usage: [ 'host' ],
+			command: function ( bot, msg ) {
+				dns.lookup( msg.args.host, function ( err, data ) {
+					if ( err ) {
+						bot.say( msg.to, 'Error:' + err + ' ' + msg.args.host );
+					}
+					bot.say( msg.to, msg.args.host + ' → ' + data );
+				} );
 			}
 		}
 	}
