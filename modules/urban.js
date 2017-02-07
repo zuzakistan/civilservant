300f5ce8a9feae130dd5cfac5dc4314ce641433f
Douglas Gardner <douglas@chippy.ch>
2017-02-07 11:01:45 +0000

    Fix output of 404 message

--- modules/urban.js
+++ modules/urban.js
@@ -11,7 +11,7 @@ module.exports = {
 				var req = urban( msg.body );
 				req.first( function ( data ) {
 					if ( typeof data === 'undefined' ) {
-						bot.say( msg.to, msg.nick + ': unable to find a definition for ' + msg.args.join( ' ' ) );
+						bot.say( msg.to, msg.nick + ': unable to find a definition for ' + msg.body );
 					} else {
 						bot.say( msg.to, msg.nick + ': ' + data.definition /* + ' ' + data.permalink */ );
 					}
