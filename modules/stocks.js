219541d5cacd713a3f6a774078aea173168fd59d
Douglas Gardner <douglas@chippy.ch>
2017-03-23 15:38:26 +0000

    Style fixes

--- modules/stocks.js
+++ modules/stocks.js
@@ -1,60 +1,46 @@
 var request = require( 'request' );
 var util = require('util');
-var delta = {};
-var id = null;
 
-var TICKER_API = 'https://foo.bar';
-var SYMBOL_API_FMT = "https://query.yahooapis.com/v1/public/yql?q=select%%20*%%20from%%20yahoo.finance.quotes%%20where%%20symbol%%20in%%20(%%22%s%%22)&format=json&env=store%%3A%%2F%%2Fdatatables.org%%2Falltableswithkeys&callback=";
-var RESULT_FMT = "%s (%s): %s (%s)";
+var SYMBOL_API_FMT = 'https://query.yahooapis.com/v1/public/yql?q=select%%20*%%20from%%20yahoo.finance.quotes%%20where%%20symbol%%20in%%20(%%22%s%%22)&format=json&env=store%%3A%%2F%%2Fdatatables.org%%2Falltableswithkeys&callback=';
+var RESULT_FMT = '%s (%s): %s (%s)';
 
 function get_sym(symbol, cb) {
-    /* cb should accept e, r { value, delta, % delta } */
-    var req = util.format(SYMBOL_API_FMT, symbol);
+	/* cb should accept e, r { value, delta, % delta } */
+	var req = util.format(SYMBOL_API_FMT, symbol);
 
-    request.get( req, function (e, r, b) {
-        if ( e ) {
-            cb (e, r, b);
-            return 'problem fetching data';
-        }
-        if ( r.statusCode !== 200 ) {
-            cb ("bad status code", r, b);
-            return 'problem fetching data (' + r.statusCode + ')';
-        }
+	request.get( req, function (e, r, b) {
+		if ( e ) {
+			cb (e, r, b);
+			return 'problem fetching data';
+		}
+		if ( r.statusCode !== 200 ) {
+			cb ('bad status code', r, b);
+			return 'problem fetching data (' + r.statusCode + ')';
+		}
 
-        var data = JSON.parse(b);
-        var symboldata = data.query.results.quote;
-        
-        cb (e, symboldata);
-    });
+		var data = JSON.parse(b);
+		var symboldata = data.query.results.quote;
+
+		cb (e, symboldata);
+	} );
 }
 
 module.exports = {
 	commands: {
 		yhoo: {
 			help: 'Displays information about a ticker symbol via yahoo',
+			usage: [ 'symbol' ],
 			command: function ( bot, msg ) {
-				if ( msg.args[1] ) {
-					id = msg.args[1];
-				}
-                
-                if ( id == null ) {
-                    bot.say( msg.to, "No previous symbol" );
-                    return;
-                }
+				get_sym( msg.args.symbol, function( e, r ) {
+					if ( e ) {
+						bot.say( msg.to, 'Problem fetching symbol' );
+					}
 
-                get_sym(id, function(e, r) {
-                    if ( e ) {
-                        return "Problem fetching symbol";
-                    }
-
-                    var line = util.format(RESULT_FMT,
-                            r.symbol,
-                            r.Name,
-                            r.Ask,
-                            r.Change_PercentChange);
-                    bot.say(msg.to, line);
-                });
+					bot.say( msg.to,
+							util.format( RESULT_FMT, r.symbol, r.Name, r.Ask, r.Change_PercentChange )
+					);
+				} );
 			}
 		}
-    }
+	}
 };
