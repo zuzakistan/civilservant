b62bf7199222ff06fdfd3bcbe359d683d7f07c34
Douglas Gardner <douglas@chippy.ch>
2017-03-29 21:50:54 +0000

    Add yakc broadcast

--- modules/udp.js
+++ modules/udp.js
@@ -1,7 +1,12 @@
 module.exports = {
 	events: {
-		udp: function ( bot, msg ) {
-			bot.notice( bot.config.udp.channel, msg.toString() );
+		udp: function ( bot, msg, rinfo ) {
+			console.log( msg, rinfo );
+			if ( bot.config.yakc && rinfo.address === bot.config.yakc.ip ) {
+				bot.fireEvents( 'yakc', msg.toString(), rinfo );
+			} else {
+				bot.notice( bot.config.udp.channel, msg.toString() );
+			}
 		}
 	}
 };
