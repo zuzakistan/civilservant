87423e43351d0a62a77705d70eb04d812a7059c6
Douglas Gardner <douglas@chippy.ch>
2017-05-16 16:38:36 +0000

    Add IMDB module

---
+++ modules/imdb.js
@@ -0,0 +1,25 @@
+var imdb = require( 'imdb-api' );
+module.exports = {
+	events: {
+		'url:imdb.com': function ( bot, url, nick, to ) {
+			var id = url.pathname.split('/').pop();
+			imdb.getReq({ id }, function ( err, movie ) {
+				if ( err ) {
+          throw err;
+        }
+				bot.shout( to, [
+					movie.title,
+					'(' + movie._year_data + ',',
+					movie.runtime + ')',
+					'·',
+					movie.rated,
+					movie.type,
+					'with rating',
+					movie.rating,
+					'[' + movie.metascore + '].',
+					movie.awards
+					].join( ' ' ) );
+			} );
+		}
+	}
+};
