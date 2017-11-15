aeb2e66762d7e3afd32e2a204e0e6cd8732565f2
Douglas Gardner <douglas@chippy.ch>
2017-11-15 19:22:27 +0000

    Delete backtick

--- modules/helpme.js
+++ modules/helpme.js
@@ -1,7 +1,7 @@
 module.exports = {
 	events: {
 		message: function ( bot, nick, to, text ) {
-			if ( text === '`help me' && Math.random() < 1/5 ) { // 1 in 5
+			if ( text === 'help me' && Math.random() < 1/5 ) { // 1 in 5
 				var interjections = [
 					'nah',
 					'no'
