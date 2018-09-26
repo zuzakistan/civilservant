a9f129102f9be80d3a3a6a75b289a8318c5e1062
Douglas Gardner <douglas@chippy.ch>
2018-09-26 20:01:05 +0000

    Add log count command

--- modules/irclog.js
+++ modules/irclog.js
@@ -5,6 +5,7 @@ module.exports = {
   commands: {
     irclog: {
       help: 'get a random irc log for a given search term',
+      aliases: ['ag'],
       command: function (bot, msg) {
         if (msg.args.length === 1) {
           return 'Usage: !irclog <search phrase>'
@@ -20,6 +21,26 @@ module.exports = {
         c.on('close', function () {
           stdout = stdout.replace(/\[30;43m/g, colors.yellow)
           stdout = stdout.replace(/\[K/g, colors.reset)
+          bot.say(msg.to, stdout)
+        })
+      }
+    },
+    irccount: {
+      help: 'count the occurences of a string in the irc logs',
+      aliases: [ 'agc', 'ircc', 'irclogc' ],
+      command: function (bot, msg) {
+        if (msg.args.length === 1) {
+          return 'Usage: !irclog <search phrase>'
+        }
+        var c = exec(esc(['ag', '-c', '--', msg.body, bot.config.logfile]))
+        var stdout = ''
+        c.stdout.on('data', function (data) {
+          stdout += data.toString()
+        })
+        c.stderr.on('data', function (data) {
+          stdout += data.toString()
+        })
+        c.on('close', function () {
           bot.say(msg.to, stdout)
         })
       }
