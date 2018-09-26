2feb3da8e52957e718d52e755d05c7be58d460f9
Daniel Evans <dan@devans.xyz>
2018-09-27 00:16:00 +0100

    Prevent hilights from irclog

--- modules/irclog.js
+++ modules/irclog.js
@@ -2,13 +2,22 @@ var exec = require('child_process').exec
 var esc = require('shell-escape')
 const colors = require('irc').colors.codes
 
-function ConvertAgSeqToIRC (str) {
+function ConvertAgToIRC (str) {
   /* eslint-disable no-control-regex */
   return str
     .replace(/\x1b\[K/g, '')
     .replace(/\x1b\[30;43m/g, colors.yellow)
     .replace(/\x1b\[0m/g, colors.reset)
   /* eslint-enable no-control-regex */
+}
+
+function PreventHilight (str) {
+  return str
+    .replace(/< ([a-zA-Z0-9_])([a-zA-Z0-9_]+)>/, '< $1\u200b$2>')
+}
+
+function ProcessAg (str) {
+  return PreventHilight(ConvertAgToIRC(str))
 }
 
 module.exports = {
@@ -29,7 +38,7 @@ module.exports = {
           stdout += data.toString()
         })
         c.on('close', function () {
-          bot.say(msg.to, ConvertAgSeqToIRC(stdout))
+          bot.say(msg.to, ProcessAg(stdout))
         })
       }
     },
