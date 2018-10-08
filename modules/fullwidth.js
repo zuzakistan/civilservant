80b59b3dc5c1ceda056fc66494bbdc16e449ec71
George Watson <george@georgewatson.me>
2018-10-08 15:33:55 +0100

    Make fullwidth conversion a function

--- modules/fullwidth.js
+++ modules/fullwidth.js
@@ -1,32 +1,36 @@
+function toFullwidth (str) {
+  let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0))
+  codePoints = codePoints.map((c) => {
+    if (c >= 33 && c <= 126) return c - 33 + 0xFF01
+    const nonAscii = {
+      32: 0x3000, // Space
+      162: 0xFFE0, // ￠
+      163: 0xFFE1, // ￡
+      165: 0xFFE5, // ￥
+      166: 0xFFE4, // ￤
+      172: 0xFFE2, // ￢
+      175: 0xFFE3, // ￣
+      8361: 0xFFE6 // ￦
+    }
+    if (c in nonAscii) return nonAscii[c]
+    return c
+  })
+  return String.fromCodePoint(...codePoints)
+}
+
 module.exports = {
   commands: {
     fullwidth: {
       help: 'Ｃｒｅａｔｅｓ　ａｎ　ａｅｓｔｈｅｔｉｃ',
       aliases: [ 'vw', 'fw' ],
       command: function (bot, msg) {
-        let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0))
-        codePoints = codePoints.map((c) => {
-          if (c >= 33 && c <= 126) return c - 33 + 0xFF01
-          const nonAscii = {
-            32: 0x3000, // Space
-            162: 0xFFE0, // ￠
-            163: 0xFFE1, // ￡
-            165: 0xFFE5, // ￥
-            166: 0xFFE4, // ￤
-            172: 0xFFE2, // ￢
-            175: 0xFFE3, // ￣
-            8361: 0xFFE6 // ￦
-          }
-          if (c in nonAscii) return nonAscii[c]
-          return c
-        })
-        return String.fromCodePoint(...codePoints)
+        return toFullwidth(msg)
       }
     }
   },
   events: {
     message: function (bot, nick, to, text) {
-      if (text.match(/^(. )+.?$/)) return bot.shout(to, fullwidth(text.replace(/(.) (.)/g,$1$2)))
+      if (text.match(/^(. )+.?$/)) return bot.shout(to, fullwidth(text.replace(/(.) (.)/g, '$1$2')))
     }
   }
 }
