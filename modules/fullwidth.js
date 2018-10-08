a8ad321ab6bc51bcc14e84a0a848ad688234e6a4
Douglas Gardner <douglas@chippy.ch>
2018-10-08 11:51:14 +0000

    Run standard --fix

--- modules/fullwidth.js
+++ modules/fullwidth.js
@@ -7,14 +7,16 @@ module.exports = {
         let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0))
         codePoints = codePoints.map((c) => {
           if (c >= 33 && c <= 126) return c - 33 + 0xFF01
-          let nonAscii = {32: 0x3000, // Space
-                     162: 0xFFE0, // ￠
-                     163: 0xFFE1, // ￡
-                     165: 0xFFE5, // ￥
-                     166: 0xFFE4, // ￤
-                     172: 0xFFE2, // ￢
-                     175: 0xFFE3, // ￣
-                     8361: 0xFFE6}; // ￦
+          let nonAscii = {
+             32: 0x3000, // Space
+            162: 0xFFE0, // ￠
+            163: 0xFFE1, // ￡
+            165: 0xFFE5, // ￥
+            166: 0xFFE4, // ￤
+            172: 0xFFE2, // ￢
+            175: 0xFFE3, // ￣
+            8361: 0xFFE6 // ￦
+          }
           if (c in nonAscii) return nonAscii[c]
           return c
         })
