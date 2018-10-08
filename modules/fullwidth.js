3f8a23d785e1036fe65315938a0dee9bb8a77eba
George Watson <george@georgewatson.me>
2018-10-08 10:25:44 +0100

    Convert spaces to U+3000 (ideographic space)

--- modules/fullwidth.js
+++ modules/fullwidth.js
@@ -7,6 +7,7 @@ module.exports = {
         let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0))
         codePoints = codePoints.map((c) => {
           if (c >= 33 && c <= 126) return c - 33 + 0xFF01
+          if (c == 32) return 0x3000
           return c
         })
         return String.fromCodePoint(...codePoints)
