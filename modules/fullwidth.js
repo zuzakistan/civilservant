426dfd692a45c0cda16009f415d3774a379b3663
Douglas Gardner <douglas@chippy.ch>
2018-10-08 10:12:36 +0100

    Only transform ascii (#108)

--- modules/fullwidth.js
+++ modules/fullwidth.js
@@ -4,7 +4,11 @@ module.exports = {
       help: 'Ｃｒｅａｔｅｓ　ａｎ　ａｅｓｔｈｅｔｉｃ',
       aliases: [ 'vw', 'fw' ],
       command: function (bot, msg) {
-        let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0) - 33 + 0xFF01)
+        let codePoints = Array.prototype.map.call(msg.body, (c) => c.codePointAt(0))
+        codePoints = codePoints.map((c) => {
+          if (c >= 33 && c <= 126) return c - 33 + 0xFF01
+          return c
+        })
         return String.fromCodePoint(...codePoints)
       }
     }
