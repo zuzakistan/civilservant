f1722a4502030ad813c220ca0094410f5e14c29a
George Watson <george@georgewatson.me>
2019-03-12 21:03:30 +0000

    Count down to Brexit at every tick of the largest unit (#145)
    * Count down to Brexit at every tick of the largest unit
    
    Maybe fix some things
    
    Keep track of the largest unit instead
    
    Simplify output
    
    Fix errors
    
    Pass 'bot' through
    
    * Pass parameters via setTimeout correctly
    
    * Use broadcast
    
    * Apply suggestions from code review
    
    Co-Authored-By: georgewatson <george@georgewatson.me>

--- modules/countdown.js
+++ modules/countdown.js
@@ -14,5 +14,17 @@ module.exports = {
         return 'Python 2.7 support ends in ' + moment('2020-01-01T00:00:00Z').countdown().toString()
       }
     }
+  },
+  onload: {
+    function (bot) {
+      function autoCount (bot, lastTick) {
+        const thisTick = moment('2019-03-29T23:00:00Z').countdown().toString().split(/, | and /)[0]
+        if (thisTick !== lastTick) {
+          bot.broadcast('Article 50 expires in ' + thisTick)
+        }
+        setTimeout(autoCount, 1000, bot, thisTick)
+      }
+      autoCount(bot)
+    }
   }
 }
