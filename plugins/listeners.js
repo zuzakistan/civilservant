a4e150164462b227939c7bed8a0b98bbddc1dd73
Daniel Evans <dan@devans.xyz>
2019-03-16 00:48:57 +0000

    add listener for join event

--- plugins/listeners.js
+++ plugins/listeners.js
@@ -15,3 +15,10 @@ bot.addListener('ctcp-version', function (from, to, message) {
 bot.addListener('action', function (from, to, text, message) {
   bot.fireEvents('action', from, to, text, message)
 })
+
+bot.addListener('join', function (channel, nick, message) {
+  bot.fireEvents('join', channel, nick, message)
+  if (nick == bot.nick) {
+    bot.fireEvents('selfjoin', channel, message)
+  }
+})
