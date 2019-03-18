8f8f8c6caec2b4e20196b54507c104be871ab45e
Daniel Evans <dan@devans.xyz>
2019-03-18 22:24:48 +0000

    Inject bot into plugins

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -1,9 +1,11 @@
-var bot = require('..')
-
-bot.broadcast = function (string) {
-  var chans = Object.keys(bot.chans)
-  console.log(chans)
-  for (var i = 0; i < chans.length; i++) {
-    bot.notice(chans[i], string)
+module.exports = {
+  onload: (bot) => {
+    bot.broadcast = function (string) {
+      var chans = Object.keys(bot.chans)
+      console.log(chans)
+      for (var i = 0; i < chans.length; i++) {
+        bot.notice(chans[i], string)
+      }
+    }
   }
 }
--- plugins/quietBot.js
+++ plugins/quietBot.js
@@ -1,11 +1,13 @@
-var bot = require('..')
-
-/*
- * Same as .say but for unprompted messages.
- * Checks a config variable to make sure it only does this in channels that want it.
- */
-bot.shout = function (target, message) {
-  if (!bot.config.get('irc.quiet')) {
-    bot.say(target, message)
+module.exports = {
+  onload: (bot) => {
+    /*
+    * Same as .say but for unprompted messages.
+    * Checks a config variable to make sure it only does this in channels that want it.
+    */
+    bot.shout = function (target, message) {
+      if (!bot.config.get('irc.quiet')) {
+        bot.say(target, message)
+      }
+    }
   }
 }
