b73304bc4e5bfdcf06cb064d04855253370232fc
Daniel Evans <dan@devans.xyz>
2019-03-18 22:24:48 +0000

    Inject bot into plugins

--- plugins/quit.js
+++ plugins/quit.js
@@ -1,9 +1,11 @@
-var bot = require('..')
-
-process.on('SIGINT', function () {
-  console.log('Caught SIGINT...')
-  bot.disconnect('Caught ^C', function () {
-    console.log('...disconnected.')
-    process.exit()
-  })
-})
+module.exports = {
+  onload: (bot) => {
+    process.on('SIGINT', function () {
+      console.log('Caught SIGINT...')
+      bot.disconnect('Caught ^C', function () {
+        console.log('...disconnected.')
+        process.exit()
+      })
+    })
+  }
+}
