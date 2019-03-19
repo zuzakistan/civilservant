ac35e6332dc3bfa2bf549a2256a5bfbeec059219
Daniel Evans <dan@devans.xyz>
2019-03-19 21:31:02 +0000

    Simplyify fireEvents

--- modules/fireEvents.js
+++ modules/fireEvents.js
@@ -2,26 +2,27 @@
  * Logic to fire events.
  */
 
+function fireEvents () {
+  var args = Array.prototype.slice.call(arguments)
+  var name = args.shift()
+
+  try { // events can fire mid-reload
+    if (!this.events[name]) return
+    for (var i = 0; i < this.events[name].length; i++) {
+      var cargs = args
+      if (cargs[0] !== this) { // getting dupes for some reason
+        cargs.unshift(this)
+      }
+      this.events[name][i].apply(this, cargs)
+    }
+  } catch (e) {
+    console.error('Error processing ' + name + ' event: ')
+    console.error(e.stack)
+  }
+}
+
 module.exports = {
   onload: (bot) => {
-    bot.fireEvents = function () {
-      var args = Array.prototype.slice.call(arguments)
-      var name = args.shift()
-
-      try { // events can fire mid-reload
-        if (bot.events[name]) {
-          for (var i = 0; i < bot.events[name].length; i++) {
-            var cargs = args
-            if (cargs[0] !== bot) { // getting dupes for some reason
-              cargs.unshift(bot)
-            }
-            bot.events[name][i].apply(this, cargs)
-          }
-        }
-      } catch (e) {
-        console.error('Error processing ' + name + ' event: ')
-        console.error(e.stack)
-      }
-    }
+    bot.fireEvents = fireEvents
   }
 }
