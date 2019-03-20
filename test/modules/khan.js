fe3f8530aaf1da26a094792df53a716406303186
Douglas Gardner <douglas@chippy.ch>
2019-03-20 11:31:13 +0000

    Add missing khan test

--- test/modules/khan.js
+++ test/modules/khan.js
@@ -10,4 +10,8 @@ describe('khan module', function () {
   it('should khan', function () {
     assert.notStrictEqual(mockBot.runCommand('!khan caw').match(/^ca+w+/i), null)
   })
+
+  it('should khan with khan by default', function () {
+    assert.notStrictEqual(mockBot.runCommand('!khan').match(/^kha+n+/i), null)
+  })
 })
