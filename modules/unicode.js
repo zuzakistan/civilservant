4926ef07d83e6e0736c9667945533edb1d900dbc
George Watson <george@georgewatson.me>
2019-03-21 12:30:27 +0000

    Make async
    Signed-off-by: George Watson <george@georgewatson.me>

--- modules/unicode.js
+++ modules/unicode.js
@@ -1,22 +1,25 @@
-var request = require('request')
+var request = require('request-promise')
 module.exports = {
   commands: {
     unicode: {
       usage: [ 'regex' ],
       aliases: [ 'char' ],
       help: 'Returns a unicode character whose name or codepoint matches a regular expression',
-      command: function (bot, msg) {
-        request.get('https://unicode.org/Public/UNIDATA/Index.txt', function (e, r, b) {
-          var data = b.split('\n')
+      command: async function (bot, msg) {
+        try {
+          const response = await request.get('https://unicode.org/Public/UNIDATA/Index.txt')
+          const data = response.split('\n')
           const regex = new RegExp(msg.args.regex, 'i')
           const matches = data.filter(codepoint => codepoint.match(regex))
           if (matches.length) {
             const thisMatch = matches[Math.floor(Math.random() * matches.length)]
-            bot.say(msg.to, msg.nick + ': ' + String.fromCodePoint('0x' + thisMatch.split('\t').slice(-1)) + '\t' + thisMatch)
+            return String.fromCodePoint('0x' + thisMatch.split('\t').slice(-1)) + '\t' + thisMatch
           } else {
-            bot.say(msg.to, msg.nick + ': No Unicode characters match /' + msg.args.regex + '/')
+            return 'No Unicode characters match /' + msg.args.regex + '/'
           }
-        })
+        } catch (e) {
+          return 'err: ' + e
+        }
       }
     }
   }
