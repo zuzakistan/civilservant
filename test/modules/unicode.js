96a34cc5bf408dfba4c92b967361b313b62e21b5
George Watson <george@georgewatson.me>
2019-03-21 12:39:50 +0000

    Add tests
    Signed-off-by: George Watson <george@georgewatson.me>

---
+++ test/modules/unicode.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+const mockBot = require('../mockBot')
+const assert = require('assert')
+
+describe('unicodemodule', function () {
+  const tests = {
+    'BUNNY': '👯 WOMAN WITH BUNNY EARS 1F46F',
+    'bunny': '👯 WOMAN WITH BUNNY EARS 1F46F',
+    '01F7': 'Ƿ WYNN, LATIN CAPITAL LETTER 01F7',
+    'Not a valid Unicode character': 'No Unicode characters match /Not a valid Unicode character/'
+  }
+
+  before(function () {
+    mockBot.loadModule('unicode')
+  })
+
+  let i = 1
+  for (const word in tests) {
+    it(`look up Unicode characters ${i++}`, function () {
+      assert.strictEqual(mockBot.runCommand(`!unicode ${word}`), tests[word])
+    })
+  }
+})
