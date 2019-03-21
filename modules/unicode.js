2d1e219f0b21fb99bd0939e5807d0bc30719546a
George Watson <george@georgewatson.me>
2019-03-21 14:32:39 +0000

    Mock-up HTTP requests in tests
    Signed-off-by: George Watson <george@georgewatson.me>

--- modules/unicode.js
+++ modules/unicode.js
@@ -2,20 +2,23 @@ var request = require('request-promise')
 module.exports = {
   commands: {
     unicode: {
-      usage: [ 'regex' ],
       aliases: [ 'char' ],
       help: 'Returns a unicode character whose name or codepoint matches a regular expression',
       command: async function (bot, msg) {
+        if (msg.args.length === 0) {
+          // This will work when (if) #162 is merged
+          msg.body = '.*'
+        }
         try {
           const response = await request.get('https://unicode.org/Public/UNIDATA/Index.txt')
           const data = response.split('\n')
-          const regex = new RegExp(msg.args.regex, 'i')
+          const regex = new RegExp(msg.body, 'i')
           const matches = data.filter(codepoint => codepoint.match(regex))
           if (matches.length) {
             const thisMatch = matches[Math.floor(Math.random() * matches.length)]
             return String.fromCodePoint('0x' + thisMatch.split('\t').slice(-1)) + '\t' + thisMatch
           } else {
-            return 'No Unicode characters match /' + msg.args.regex + '/'
+            return 'No Unicode characters match /' + msg.body + '/'
           }
         } catch (e) {
           return 'err: ' + e
--- test/modules/unicode.js
+++ test/modules/unicode.js
@@ -1,28 +1,37 @@
 /* eslint-env mocha */
 const mockBot = require('../mockBot')
 const assert = require('assert')
+const mockery = require('mockery')
 
-describe('unicodemodule', function () {
-  const tests = {
-    'BUNNY': '👯 WOMAN WITH BUNNY EARS 1F46F',
-    'bunny': '👯 WOMAN WITH BUNNY EARS 1F46F',
-    '01F7': 'Ƿ WYNN, LATIN CAPITAL LETTER 01F7',
-    'Not a valid Unicode character': 'No Unicode characters match /Not a valid Unicode character/'
-  }
-
+const data = 'WOMAN WITH BUNNY EARS\t1F46F\nWYNN, LATIN CAPITAL LETTER\t01F7'
+const tests = {
+  'BUNNY': '👯\tWOMAN WITH BUNNY EARS\t1F46F',
+  'bunny': '👯\tWOMAN WITH BUNNY EARS\t1F46F',
+  'latin': 'Ƿ\tWYNN, LATIN CAPITAL LETTER\t01F7',
+  '1F46F': '👯\tWOMAN WITH BUNNY EARS\t1F46F',
+  'Not a bunny': 'No Unicode characters match /Not a bunny/'
+}
+describe('unicode module', function () {
   before(function () {
+    mockery.enable()
+    mockery.registerMock('request-promise', {
+      get: () => Promise.resolve(data)
+    })
+
     mockBot.loadModule('unicode')
   })
 
   let i = 1
   for (const word in tests) {
-    it(`look up Unicode characters ${i++}`, async function () {
+    it(`should find matching characters (${i++})`, async function () {
       let promise = await mockBot.runCommand(`!unicode ${word}`)
       assert.strictEqual(promise, tests[word])
     })
-  }
 
-  after(function (done) {
-    done()
-  })
+    after(function (done) {
+      mockery.disable()
+      mockery.deregisterAll()
+      done()
+    })
+  }
 })
