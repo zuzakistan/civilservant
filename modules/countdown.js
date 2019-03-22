766fcef11aac2f496fcb5d25624d615adf11b6dc
Douglas Gardner <douglas@chippy.ch>
2019-03-22 09:50:24 +0000

    Add GE counter, extended A50 counter (#194)

--- modules/countdown.js
+++ modules/countdown.js
@@ -19,6 +19,14 @@ module.exports = {
         return 'Article 50 expires in ' + moment('2019-03-29T23:00:00Z').countdown().toString()
       }
     },
+    a50e: {
+      help: 'Gets the time until the UK Article 50 procedure expires',
+      command: () => 'Article 50, as extended, expires in ' + moment('2019-05-22T23:00:00Z').countdown().toString()
+    },
+    ge: {
+      help: 'Gets the time until the next General Election under FTPA',
+      command: () => 'Polls open in ' + moment('2022-05-05T08:00:00Z').countdown().toString()
+    },
     python2: {
       help: 'Gets the time until Python 2 support is dropped',
       command: function () {
