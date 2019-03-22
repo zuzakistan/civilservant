1cb4e6d84cff124911785f9f12901d2a994c4dc8
Douglas Gardner <douglas@chippy.ch>
2019-03-22 23:45:04 +0000

    Refactor space.js
    - Use async because it's the future
    - Use .map instead of looping because it's the future

--- modules/space.js
+++ modules/space.js
@@ -1,26 +1,16 @@
-var request = require('request')
+const request = require('request-promise')
 module.exports = {
   commands: {
     space: {
       help: 'Displays the people in space right now',
-      command: function (bot, msg) {
-        request.get('http://www.howmanypeopleareinspacerightnow.com/peopleinspace.json', function (e, r, b) {
-          if (e) {
-            bot.say(msg.to, 'problem fetching data')
-          }
-          if (r.statusCode !== 200) {
-            bot.say(msg.to, 'problem fetching data (' + r.statusCode + ')')
-          }
-          var data = JSON.parse(b)
-          var ret = []
-          for (var i = 0; i < data.people.length; i++) {
-            if (data.people[i].country === 'England') { // pet peeve
-              data.people[i].country = 'United Kingdom'
-            }
-            ret.push(data.people[i].name + ' (' + data.people[i].country + ')')
-          }
-          bot.say(msg.to, msg.nick + ': ' + ret.join('; '))
+      command: async function (bot, msg) {
+        const b = await request.get('http://www.howmanypeopleareinspacerightnow.com/peopleinspace.json')
+        const data = JSON.parse(b)
+        let ret = data.people.map((person) => {
+          if (person.country === 'England') person.country = 'United Kingdom' // pet peeve
+          return `${person.name} (${person.country})`
         })
+        return ret.join('; ')
       }
     }
   }
