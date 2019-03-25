0d8dc0a12fa90279e4f8129d5021970f625a630a
George Watson <george@georgewatson.me>
2019-03-25 14:01:40 +0000

    Add comments

--- modules/unicode.js
+++ modules/unicode.js
@@ -5,17 +5,23 @@ module.exports = {
       aliases: [ 'char' ],
       help: 'Returns a unicode character whose name or codepoint matches a regular expression',
       command: async function (bot, msg) {
+        // Output a random character if no arguments are given
         if (msg.args.length === 0) {
           // This will work when (if) #162 is merged
           msg.body = '.*'
         }
         try {
+          // Get data from Unicode
           const response = await request.get('https://unicode.org/Public/UNIDATA/UnicodeData.txt')
           const data = response.split('\n')
+          // Find matching characters
           const regex = new RegExp(msg.body, 'i')
           const matches = data.filter(codepoint => codepoint.match(regex))
           if (matches.length) {
+            // Choose a random match
             const thisMatch = matches[Math.floor(Math.random() * matches.length)].split(';')
+            // Print it only if it is not a control character or a line/paragraph spacer (categories C, Zl, Zp)
+            // Otherwise, only print its metadata
             let theChar = ''
             if (!thisMatch[2].match(/(C.|Z[lp])/)) {
               theChar = String.fromCodePoint('0x' + thisMatch[0]) + ' '
--- test/modules/unicode.js
+++ test/modules/unicode.js
@@ -5,15 +5,23 @@ const mockery = require('mockery')
 
 const data = '0000;<control>;Cc;0;BN;;;;;N;NULL;;;;\n1F46F;WOMAN WITH BUNNY EARS;So;0;ON;;;;;N;;;;;\n0391;GREEK CAPITAL LETTER ALPHA;Lu;0;L;;;;;N;;;;03B1;\n03B1;GREEK SMALL LETTER ALPHA;Ll;0;L;;;;;N;;;0391;;0391'
 const tests = {
+  // Basic search
   'BUNNY': '👯 WOMAN WITH BUNNY EARS U+1F46F',
   'bunny': '👯 WOMAN WITH BUNNY EARS U+1F46F',
+  // Multiple words
   'greek small': 'α GREEK SMALL LETTER ALPHA U+03B1',
+  // Regex
+  'gr.*k small': 'α GREEK SMALL LETTER ALPHA U+03B1',
+  // Search by codepoint
   '1F46F': '👯 WOMAN WITH BUNNY EARS U+1F46F',
+  // Unprintable
   'null': '<control> U+0000',
+  // Failed search
   'Not a bunny': 'No Unicode characters match /Not a bunny/'
 }
 describe('unicode module', function () {
   before(function () {
+    // Mock up the HTTP request
     mockery.enable()
     mockery.registerMock('request-promise', {
       get: () => Promise.resolve(data)
