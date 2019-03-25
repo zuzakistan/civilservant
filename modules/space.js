9b938db3cd22255a2a66fc1b62d5f61dc54af0a9
Douglas Gardner <douglas@chippy.ch>
2019-03-25 22:09:18 +0000

    Refactor iss module (#210)
    * Rename iss.js as it's not the only one
    - Use async because it's the future
    - Use .map instead of looping because it's the future
    
    * Add space tests
    
    * Fix casing

---
+++ modules/space.js
@@ -0,0 +1,17 @@
+const request = require('request-promise')
+module.exports = {
+  commands: {
+    space: {
+      help: 'Displays the people in space right now',
+      command: async function (bot, msg) {
+        const b = await request.get('http://www.howmanypeopleareinspacerightnow.com/peopleinspace.json')
+        const data = JSON.parse(b)
+        let ret = data.people.map((person) => {
+          if (person.country === 'england') person.country = 'united kingdom' // pet peeve
+          return `${person.name} (${person.country})`
+        })
+        return ret.join('; ')
+      }
+    }
+  }
+}
