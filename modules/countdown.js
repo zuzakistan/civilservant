4d7c2f317240100539687ed97e58fbacd7b3c91a
Douglas Gardner <douglas@chippy.ch>
2019-03-26 13:28:49 +0000

    Implement EUCO XT 20006/19
    Change date in accordance with the European Council decision taken in
    agreement with the United Kingdom, extending the period under Article
    50(3)TEU

--- modules/countdown.js
+++ modules/countdown.js
@@ -3,8 +3,10 @@ require('moment-countdown')
 
 let timeout = null
 
+const ARTICLE_50 = '2019-04-19T23:00:00Z'
+
 function autoCount (bot, lastTick) {
-  const thisTick = moment('2019-03-29T23:00:00Z').countdown().toString().split(/, | and /)[0]
+  const thisTick = moment(ARTICLE_50).countdown().toString().split(/, | and /)[0]
   if (lastTick != null && thisTick !== lastTick) {
     bot.broadcast('Article 50 expires in ' + thisTick)
   }
@@ -16,7 +18,7 @@ module.exports = {
     a50: {
       help: 'Gets the time until the UK Article 50 procedure expires',
       command: function () {
-        return 'Article 50 expires in ' + moment('2019-03-29T23:00:00Z').countdown().toString()
+        return 'Article 50 expires in ' + moment(ARTICLE_50).countdown().toString()
       }
     },
     a50e: {
