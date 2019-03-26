8fe577c69c3bc400d9ceed5ab712842a86f153a6
Douglas Gardner <douglas@chippy.ch>
2019-03-26 12:53:36 +0000

    Add Twitter feed for owo'd news stories

--- modules/twitter.js
+++ modules/twitter.js
@@ -1,5 +1,6 @@
 const TwitterPin = require('twitter-pin')
 const Tweeter = require('fast-tweet')
+const owo = require('@zuzak/owo')
 let twitterPin
 
 module.exports = {
@@ -30,6 +31,7 @@ module.exports = {
         })
         bot.config.set(`twitter.users.${response.screen_name}.key`, response.token)
         bot.config.set(`twitter.users.${response.screen_name}.secret`, response.secret)
+        bot.say(msg.nick, JSON.stringify(response))
         return 'authorized as ' + response.screen_name
       }
     },
@@ -48,6 +50,20 @@ module.exports = {
         let tweet = await client.tweet({ status: msg.body })
         return 'https://twitter.com/statuses/' + tweet.id_str
       }
+    }
+  },
+  events: {
+    newNews: (bot, news) => {
+      let user = bot.config.get('twitter.newsUser')
+      if (!user) return 'tweeting disabled'
+      let client = new Tweeter({
+        consumer_key: bot.config.get('twitter.keys.consumerKey'),
+        consumer_secret: bot.config.get('twitter.keys.consumerSecret'),
+        access_token_key: bot.config.get(`twitter.users.${user}.key`),
+        access_token_secret: bot.config.get(`twitter.users.${user}.secret`)
+      })
+      let url = news.url ? news.url : ''
+      client.tweet({ status: owo(news.text) + '\r\n' + url })
     }
   }
 }
