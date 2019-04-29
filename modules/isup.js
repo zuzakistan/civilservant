ea660deaba85a90447bef31b1804a137cbd21245
Douglas Gardner <douglas@chippy.ch>
2019-04-29 13:05:16 +0000

    Remove try/catch
    Thanks to #209 we can kill this entirely

--- modules/isup.js
+++ modules/isup.js
@@ -7,27 +7,23 @@ module.exports = {
       help: 'Performs a HTTP(S) request to an arbitrary host.',
       usage: [ 'host' ],
       command: async function (bot, msg) {
-        try {
-          const res = await request({ url: msg.args.host, resolveWithFullResponse: true })
-          let str = 'HTTP/' + res.httpVersion
-          str += ' ' + res.req.method + ' ' + res.request.href
-          str += ' → ' + res.statusCode
+        const res = await request({ url: msg.args.host, resolveWithFullResponse: true })
+        let str = 'HTTP/' + res.httpVersion
+        str += ' ' + res.req.method + ' ' + res.request.href
+        str += ' → ' + res.statusCode
 
-          let flag = ''
-          try {
-            let headers = [
-              res.headers['content-type'],
-              res.headers.server,
-              res.body.length + 'B'
-            ]
-            str += ' ' + headers.join(' · ')
-          } catch (e) {
-            flag = '!'
-          }
-          return str + flag
+        let flag = ''
+        try {
+          let headers = [
+            res.headers['content-type'],
+            res.headers.server,
+            res.body.length + 'B'
+          ]
+          str += ' ' + headers.join(' · ')
         } catch (e) {
-          return 'err: ' + e
+          flag = '!'
         }
+        return str + flag
       }
     }
   }
