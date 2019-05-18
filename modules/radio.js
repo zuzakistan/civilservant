ae03c7f0355eaf36f8ddc2d9fa433117da01d9f6
Douglas Gardner <douglas@chippy.ch>
2019-05-18 16:08:36 +0000

    Add TV schedule commands

--- modules/radio.js
+++ modules/radio.js
@@ -2,6 +2,56 @@ const request = require('request-promise')
 const moment = require('moment')
 const colors = require('irc').colors
 
+const tv = async (station) => {
+  const url = `https://ibl.api.bbci.co.uk/ibl/v1/channels/${station}/broadcasts?per_page=2`
+  let data
+  let str
+  const response = await request.get(url)
+  try {
+    data = JSON.parse(response)
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      return response
+    }
+    throw e
+  }
+
+  const now = data.broadcasts.elements[0]
+  const next = data.broadcasts.elements[1]
+
+  str = [
+    now.episode.live ? 'Live' : 'Now',
+    'on',
+    data.broadcasts.channel.title + ':',
+    colors.wrap(now.episode.master_brand.id === 'bbc_sport' ? 'yellow' : 'white', now.episode.title),
+    now.episode.subtitle ? colors.wrap('light_gray', '(' + now.episode.subtitle + ')') : null,
+    colors.wrap('gray', ((ep) => {
+      return [
+        ep.signed ? 'S' : null,
+        ep.guidance ? 'G' : null,
+        ep.audio_described ? 'AD' : null,
+        now.repeat ? 'R' : null
+      ].filter(x => x != null).join(' ')
+    })(now.episode)),
+    'followed by',
+    next.repeat ? 'a repeat of' : null,
+    next.episode.title,
+    moment(next.scheduled_start).fromNow(),
+    moment(next.scheduled_start).format('(h:mm a)')
+  ].filter(x => x != null)
+
+  if (now.transmission_start && now.scheduled_start !== now.transmission_start) {
+    let paperStart = moment(now.scheduled_start)
+    let actualStart = moment(now.transmission_start)
+    str.push(colors.wrap('light_red',
+      'overrunning by ' + moment.duration(actualStart.diff(paperStart)).humanize()
+    ))
+  }
+  if (!data.broadcasts.channel.on_air) str.push(colors.wrap('light_cyan', 'off-air'))
+
+  return str.join(' ')
+}
+
 const radio = async (station) => {
   const url = `https://polling.bbc.co.uk/modules/onairpanel/include/${station}.json`
   let str
@@ -37,6 +87,39 @@ const radio = async (station) => {
 
 module.exports = {
   commands: {
+    // https://github.com/MikeRalphson/bbcparse/wiki/BBC-Linear-Services-(SIDs)
+    bbc1: {
+      aliases: [ 'bbcone' ],
+      help: 'Displays the current BBC One schedule',
+      command: async (bot, msg) => tv('bbc_one_hd')
+    },
+    bbc2: {
+      aliases: [ 'bbc2e', 'bbctwo' ],
+      help: 'Displays the current BBC Two schedule',
+      command: async (bot, msg) => tv('bbc_two_england')
+    },
+    bbc4: {
+      aliases: [ 'bbcfour' ],
+      help: 'Displays the current BBC Four schedule',
+      command: async (bot, msg) => tv('bbc_four')
+    },
+    bbcparl: {
+      aliases: [ 'bbcp', 'parly' ],
+      help: 'Displays the current BBC Parliament schedule',
+      command: async (bot, msg) => tv('bbc_parliament')
+    },
+    cbbc: {
+      help: 'Displays what\'s on CBBC right now',
+      command: async (bot, msg) => tv('cbbc')
+    },
+    cbeebies: {
+      help: 'Displays what\'s on CBeebies right now',
+      command: async (bot, msg) => tv('cbeebies')
+    },
+    bbcn: {
+      help: 'Displays what\'s on BBC News HD right now',
+      command: async (bot, msg) => tv('bbc_news_channel_hd')
+    },
     worldservice: {
       help: 'Displays the current BBC World Service schedule',
       command: async (bot, msg) => radio('bbc_world_service')
@@ -60,6 +143,11 @@ module.exports = {
     r4lw: {
       help: 'Displays the current BBC Radio 4 long-wave schedule',
       command: async (bot, msg) => radio('bbc_radio_fourlw')
+    },
+    tv: {
+      help: 'Displays the schedule for an arbitary BBC television channel',
+      usage: [ 'channel' ],
+      command: async (bot, msg) => tv(msg.args.channel)
     },
     radio: {
       help: 'Displays the schedule for an arbitary BBC Radio station',
