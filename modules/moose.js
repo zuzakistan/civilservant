b815bc8f3d5e276b1d3ef0dfb575792060e34850
George Watson <george@georgewatson.me>
2019-08-30 11:00:32 +0100

    Add Swedish translation

---
+++ modules/moose.js
@@ -0,0 +1,17 @@
+module.exports = {
+  commands: {
+    moose: {
+      help: 'A møøse once bit my sister',
+      aliases: [ 'swedish' ],
+      command: function (bot, msg) {
+        return msg.body
+          .replace(/\band\b|ith\b/g, 'ik')
+          .replace(/\bthe\b/g, 'den')
+          .replace(/o/g, 'ø')
+          .replace(/h?y\b/g, 'i')
+          .replace(/\bC/g, 'K')
+          .replace(/\bc/g, 'k')
+      }
+    }
+  }
+}
