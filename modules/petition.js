61d5cff08c9f699f563ca08f11c30821cc7e4be0
Douglas Gardner <douglas@chippy.ch>
2019-08-31 23:07:01 +0000

    Remove giant try/catch
    Code to echo an error message is now elsewhere, which means we can get
    rid of giant try/catch blocks solely to output a message to the user.
    
    This commit removes that giant try/catch and only sticks the try around
    the bit of code we're actually explicitly catching.

--- modules/petition.js
+++ modules/petition.js
@@ -22,82 +22,6 @@ module.exports = {
             }
           })
           data = data.data
-
-          const old = delta[data.id] ? delta[data.id] : data.attributes.signature_count
-          const change = data.attributes.signature_count - old
-          delta[data.id] = old + change
-
-          var domestic = 0
-          var foreign = 0
-          if (data.attributes.signatures_by_country) {
-            data.attributes.signatures_by_country.forEach(country => {
-              if (country.code === 'GB') {
-                domestic += country.signature_count
-              } else {
-                foreign += country.signature_count
-              }
-            })
-          }
-          const ELECTORATE_SIZE = 46800000 // PGE 2017
-          const percentageOfElectorate = (domestic / ELECTORATE_SIZE * 100).toPrecision(3) + '%'
-          const surplusSignatures = data.attributes.signature_count - domestic - foreign
-
-          let ret
-          switch (data.attributes.state) {
-            case 'rejected':
-              ret = [
-                colors.wrap('light_red', data.attributes.rejection.code),
-                data.type,
-                data.id,
-                colors.wrap('white', '"' + data.attributes.action + '"'),
-                'https://petition.parliament.uk/petitions/' + data.id
-              ]
-              break
-            case 'closed':
-              ret = [
-                colors.wrap('dark_red', data.attributes.state),
-                data.type,
-                data.id,
-                colors.wrap('white', '"' + data.attributes.action + '"'),
-                'had',
-                data.attributes.signature_count.toLocaleString('en-GB'),
-                'signatures',
-                '·',
-                percentageOfElectorate,
-                'of electorate',
-                '(' + domestic.toLocaleString('en-GB') + ')',
-                'plus',
-                foreign.toLocaleString('en-GB'),
-                'foreign',
-                surplusSignatures > 0 ? 'and ' + surplusSignatures.toLocaleString('en-GB') + ' unaccounted for' : null,
-
-                'https://petition.parliament.uk/petitions/' + data.id
-              ]
-              break
-            case 'open':
-            default:
-              ret = [
-                colors.wrap('light_green', data.attributes.state),
-                data.type,
-                data.id,
-                colors.wrap('white', '"' + data.attributes.action + '"'),
-                'has',
-                data.attributes.signature_count.toLocaleString('en-GB'),
-                'signatures',
-                colors.wrap('yellow', '(+' + change.toLocaleString('en-GB') + ')'),
-                '·',
-                percentageOfElectorate,
-                'of electorate',
-                '(' + domestic.toLocaleString('en-GB') + ')',
-                'plus',
-                foreign.toLocaleString('en-GB'),
-                'foreign',
-                surplusSignatures > 0 ? 'and ' + surplusSignatures.toLocaleString('en-GB') + ' unaccounted for' : null,
-
-                'https://petition.parliament.uk/petitions/' + data.id
-              ]
-          }
-          return ret.join(' ')
         } catch (e) {
           // if HTTP error intercept to remove body of response
           if (e.name === 'StatusCodeError') {
@@ -106,6 +30,82 @@ module.exports = {
           // rethrow anyway
           throw e
         }
+
+        const old = delta[data.id] ? delta[data.id] : data.attributes.signature_count
+        const change = data.attributes.signature_count - old
+        delta[data.id] = old + change
+
+        var domestic = 0
+        var foreign = 0
+        if (data.attributes.signatures_by_country) {
+          data.attributes.signatures_by_country.forEach(country => {
+            if (country.code === 'GB') {
+              domestic += country.signature_count
+            } else {
+              foreign += country.signature_count
+            }
+          })
+        }
+        const ELECTORATE_SIZE = 46800000 // PGE 2017
+        const percentageOfElectorate = (domestic / ELECTORATE_SIZE * 100).toPrecision(3) + '%'
+        const surplusSignatures = data.attributes.signature_count - domestic - foreign
+
+        let ret
+        switch (data.attributes.state) {
+          case 'rejected':
+            ret = [
+              colors.wrap('light_red', data.attributes.rejection.code),
+              data.type,
+              data.id,
+              colors.wrap('white', '"' + data.attributes.action + '"'),
+              'https://petition.parliament.uk/petitions/' + data.id
+            ]
+            break
+          case 'closed':
+            ret = [
+              colors.wrap('dark_red', data.attributes.state),
+              data.type,
+              data.id,
+              colors.wrap('white', '"' + data.attributes.action + '"'),
+              'had',
+              data.attributes.signature_count.toLocaleString('en-GB'),
+              'signatures',
+              '·',
+              percentageOfElectorate,
+              'of electorate',
+              '(' + domestic.toLocaleString('en-GB') + ')',
+              'plus',
+              foreign.toLocaleString('en-GB'),
+              'foreign',
+              surplusSignatures > 0 ? 'and ' + surplusSignatures.toLocaleString('en-GB') + ' unaccounted for' : null,
+
+              'https://petition.parliament.uk/petitions/' + data.id
+            ]
+            break
+          case 'open':
+          default:
+            ret = [
+              colors.wrap('light_green', data.attributes.state),
+              data.type,
+              data.id,
+              colors.wrap('white', '"' + data.attributes.action + '"'),
+              'has',
+              data.attributes.signature_count.toLocaleString('en-GB'),
+              'signatures',
+              colors.wrap('yellow', '(+' + change.toLocaleString('en-GB') + ')'),
+              '·',
+              percentageOfElectorate,
+              'of electorate',
+              '(' + domestic.toLocaleString('en-GB') + ')',
+              'plus',
+              foreign.toLocaleString('en-GB'),
+              'foreign',
+              surplusSignatures > 0 ? 'and ' + surplusSignatures.toLocaleString('en-GB') + ' unaccounted for' : null,
+
+              'https://petition.parliament.uk/petitions/' + data.id
+            ]
+        }
+        return ret.join(' ')
       }
     }
   }
