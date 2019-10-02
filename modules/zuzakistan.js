215e5bbd7852b4334a6106d68629607f4aaf9622
Douglas Gardner <douglas@chippy.ch>
2019-10-02 15:57:54 +0000

    Add quick link to log readout

---
+++ modules/zuzakistan.js
@@ -0,0 +1,8 @@
+module.exports = {
+  commands: {
+    logs: {
+      help: 'Returns link to logs page on github.com/zuzak/gov.zk',
+      command: () => 'https://zuzakistan.com/civil-servant/console'
+    }
+  }
+}
