3e8a23b765f56ab4eec0ae8f6e0144ab5411ab04
Douglas Gardner <douglas@chippy.ch>
2019-10-02 13:39:10 +0000

    Fix verbose logger

--- modules/verbose.js
+++ modules/verbose.js
@@ -1,9 +1,7 @@
 module.exports = {
   events: {
     raw: function (bot, msg) {
-      if (bot.config.get('irc.verbose')) {
         bot.log('verbose', msg.rawCommand + ' (' + msg.command + ') ' + msg.args.join(' '))
-      }
     }
   }
 }
