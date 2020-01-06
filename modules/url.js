b60d4d37201dc69ae266cfa8a427fbb7fe7b70b2
Douglas Gardner <douglas@chippy.ch>
2020-01-06 13:55:40 +0000

    Remove deprecated url.parse

--- modules/url.js
+++ modules/url.js
@@ -1,5 +1,4 @@
 var getUrls = require('get-urls')
-var parseurl = require('url')
 module.exports = {
   events: {
     message: function (bot, nick, to, text, msg) {
@@ -7,7 +6,7 @@ module.exports = {
       if (urls.size) {
         bot.fireEvents('urls', urls, nick, to, text, msg)
         for (const url of urls) {
-          const curr = parseurl.parse(url)
+          const curr = new URL(url)
           bot.fireEvents('url', curr, nick, to, text, msg)
           bot.fireEvents('url:' + curr.hostname, curr, nick, to, text, msg)
         }
