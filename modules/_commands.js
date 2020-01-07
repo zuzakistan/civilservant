zuzakistan/civilservant

commit bd39403833e00f857851e000f50d90a3ead89a41

Remove direct calls to Object.prototype builtins
This implements the change made in StandardJS 12.

For more details see:
https://eslint.org/docs/rules/no-prototype-builtins

modules/_commands.js
@@ -33,7 +33,7 @@ module.exports = {
           message: message
         }
         msg._cmd = controlChar + msg.args[0]
-        if (bot.commands.hasOwnProperty(msg.args[0])) {
+        if (Object.prototype.hasOwnProperty.call(bot, msg.args[0])) {
           try {
             var cmd = bot.commands[msg.args[0]]
             if (cmd.command) {

modules/count.js
@@ -17,7 +17,7 @@ module.exports = {
         } else {
           counter = counter.join(' ')
         }
-        if (!count.hasOwnProperty(counter)) {
+        if (!Object.prototype.hasOwnProperty.call(count, counter)) {
           count[counter] = 1
         } else {
           count[counter]++

modules/govuk.js
@@ -24,7 +24,7 @@ module.exports = {
         })
         const orgs = []
         for (const prop in data) {
-          if (data.hasOwnProperty(prop)) {
+          if (Object.prototype.hasOwnProperty.call(data, prop)) {
             orgs.push(data[prop])
           }
         }

modules/yakc.js
@@ -26,7 +26,7 @@ module.exports = {
             bot.log('silly', actions)
             var str = []
             for (var action in actions) {
-              if (!actions.hasOwnProperty(action)) {
+              if (!Object.prototype.hasOwnProperty.call(actions, action)) {
                 continue
               }
 

commit c1792431d29f0ca69e8191b15390f1662f6767ab

Fix command checking
Fixes regression introduced in bd39403833e00f857851e000f50d90a3ead89a41

modules/_commands.js
@@ -33,7 +33,7 @@ module.exports = {
           message: message
         }
         msg._cmd = controlChar + msg.args[0]
-        if (Object.prototype.hasOwnProperty.call(bot, msg.args[0])) {
+        if (Object.prototype.hasOwnProperty.call(bot.commands, msg.args[0])) {
           try {
             var cmd = bot.commands[msg.args[0]]
             if (cmd.command) {