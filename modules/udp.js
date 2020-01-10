36a42af4fa6a02fa45c357020269ca52ca9e27d1
Douglas Gardner <douglas@chippy.ch>
2020-01-10 15:00:09 +0000

    Copy interesting tweets to control channel

--- modules/udp.js
+++ modules/udp.js
@@ -5,6 +5,11 @@ module.exports = {
         bot.fireEvents('yakc', msg.toString(), rinfo)
       } else {
         bot.notice(bot.config.get('udp.channel'), msg.toString())
+
+        // copy https://github.com/zuzak/twitter-irc-udp messages from verified users:
+        if (msg.toString().includes('✓')) {
+          bot.notice(bot.config.get('irc.control'), msg.toString())
+        }
       }
     }
   }
