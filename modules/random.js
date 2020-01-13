78b2e13db91d337399085617c8e4570df63ad250
Guy Thompson <guy.thompson@emishealth.com>
2020-01-13 10:24:06 +0000

    Update random.js
    Fix for #396 (not 100% confident in this fix)

--- modules/random.js
+++ modules/random.js
@@ -3,13 +3,16 @@ module.exports = {
     random: {
       help: 'Selects a random choice or number',
       command: function (bot, msg) {
+        var randint
+        var randstring = randint.toString()
         if (msg.args.length > 2) {
           msg.args.shift()
-          return msg.args[Math.floor(Math.random() * msg.args.length)]
+          randint = msg.args[Math.floor(Math.random() * msg.args.length)]
+          return randstirng
         } else if (msg.args.length === 2) {
-          var res = Math.floor(Math.random() * Math.floor(msg.args[1]))
+          randint = Math.floor(Math.random() * Math.floor(msg.args[1]))
           if (!isNaN(res)) {
-            return res
+            return randstring
           }
         }
         return 'Usage: (<number>|<choice 1> … <choice n>)'
