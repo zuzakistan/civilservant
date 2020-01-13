916c280219e88ba49ebb8cd77d5fbdb83893d6d1
Guy Thompson <guy.thompson@emishealth.com>
2020-01-13 10:24:06 +0000

    Fix for #396
    (not 100% confident in this fix)

--- modules/random.js
+++ modules/random.js
@@ -5,7 +5,8 @@ module.exports = {
       command: function (bot, msg) {
         if (msg.args.length > 2) {
           msg.args.shift()
-          return msg.args[Math.floor(Math.random() * msg.args.length)]
+          const randint = msg.args[Math.floor(Math.random() * msg.args.length)]
+          return randint.toString()
         } else if (msg.args.length === 2) {
           var res = Math.floor(Math.random() * Math.floor(msg.args[1]))
           if (!isNaN(res)) {
