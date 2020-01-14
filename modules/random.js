5b6280087b406ee159b342df933b12940e260704
Guy Thompson <47520414+G-Thompson-Ops@users.noreply.github.com>
2020-01-14 10:49:47 +0000

    Fix conversion to String from Number (#401)
    * Update random.js
    
    Fixed #396 - Removed pointless conversion and var and added conversion to var 'res' - This is because the handler can't interpreit var's with properties other than string.
    
    This is a greater problem and should probably be re-visitied.
    
    * Update random.js

--- modules/random.js
+++ modules/random.js
@@ -5,12 +5,11 @@ module.exports = {
       command: function (bot, msg) {
         if (msg.args.length > 2) {
           msg.args.shift()
-          const randint = msg.args[Math.floor(Math.random() * msg.args.length)]
-          return randint.toString()
+          return msg.args[Math.floor(Math.random() * msg.args.length)]
         } else if (msg.args.length === 2) {
           var res = Math.floor(Math.random() * Math.floor(msg.args[1]))
           if (!isNaN(res)) {
-            return res
+            return res.toString()
           }
         }
         return 'Usage: (<number>|<choice 1> … <choice n>)'
