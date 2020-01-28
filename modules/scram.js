1b97489f36b8e64be0385b6ba77c961812876abc
Douglas Gardner <douglas@chippy.ch>
2020-01-28 12:37:58 +0000

    Automatically retweet some Weutews stories (#416)
    * Automatically retweet some Weutews stories
    
    * Add scram functionality

---
+++ modules/scram.js
@@ -0,0 +1,31 @@
+const request = require('request-promise')
+
+module.exports = {
+  onload: (bot) => {
+    bot.isNotDisabled = async () => {
+      bot.log('debug', 'Checking GOV.ZK to see if we can tweet')
+      const data = await request.get({
+        url: `${bot.config.get('govzk.endpoint')}/civil-servant/scram.json?key=${bot.config.get('govzk.key')}`,
+        json: true,
+        maxRedirects: 0
+      })
+      bot.log('debug', 'GOV.ZK Result: ' + JSON.stringify(data))
+      if (data === 'ok') return true
+      if (data.reason && data.user) {
+        throw new Error(`Scram enabled by ${data.user}: ${data.reason}`)
+      }
+      throw new Error('Bot is blocked')
+    }
+  },
+  commands: {
+    scrammed: {
+      help: 'Checks if we can tweet',
+      command: async (bot) => (await bot.isNotDisabled()) ? 'not disabled' : 'disabled'
+    },
+    stop: {
+      alias: ['scram'],
+      help: 'Links to the emergency stop interface',
+      command: () => 'https://zuzakistan.com/civil-servant/scram'
+    }
+  }
+}
